b49b6263ac18701ef48efad3ccd818ec39129aa3
austy <austincheckshismail@gmail.com>
2021-12-16 21:30:43 -0600

    seventh commit

---
+++ jwt/index.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const token = jwt.sign({ id: 3, username: 'joshua' }, 'server secret', { expiresIn: '1w' });
+
+token;
+
+const recoveredData = jwt.verify(token, 'server secret');
+
+recoveredData;
+
+jwt.verify(token, 'server secret');
+
+module.exports = {
+  token,
+};
