fcc96ae463b210fdecae1a93e65c1e245887b7c4
austy <austincheckshismail@gmail.com>
2021-12-20 10:11:59 -0600

    deploying first API!

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,7 @@
+const { post } = require('curl');
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log('A request is being made to /tags');
@@ -8,12 +9,33 @@ tagsRouter.use((req, res, next) => {
   next();
 });
 
-tagsRouter.get('/', async (req, res) => {
-  const tags = await getAllTags();
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
 
-  res.send({
-    tags,
-  });
+  try {
+    // use our method to get posts by tag name from the db
+    const posts = await getPostsByTagName(tagName);
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts: posts });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+tagsRouter.get('/', async (req, res) => {
+  try {
+    const allTags = await getAllTags();
+
+    const tags = allTags.filter((tag) => {
+      return tag.active || (req.user && tag.author.id === req.user.id);
+    });
+    res.send({
+      tags,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
